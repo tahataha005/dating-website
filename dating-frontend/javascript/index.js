7892e5cdf246b565188506cbe08d99498e32f21f
tahataha005 <taha.f.taha0@gmail.com>
2022-10-04 00:23:48 +0300

    Frontend: Creating js structure functions to be used in all pages and implementing index.html load function(load_registration)

--- dating-frontend/javascript/index.js
+++ dating-frontend/javascript/index.js
@@ -1,13 +1,62 @@
-const login_container = document.getElementById("login-content")
-const signup_container = document.getElementById("signup-content")
-const login_content_btn = document.getElementById("login-content-button");
-const signup_content_btn = document.getElementById("signup-content-btn");
-console.log(login_content_btn);
+const pages = {};
+pages.baseURL = "http://127.0.0.1:8000/api";
 
-const registration_content = () => {
-    login_container.classList.toggle("hide");
-    signup_container.classList.toggle("hide");
+pages.Console = (title, values, oneValue = true) => {
+    console.log('---' + title + '---');
+    if(oneValue){
+        console.log(values);
+    }else{
+        for(let i =0; i< values.length; i++){
+            console.log(values[i]);
+        }
+    }
+    console.log('--/' + title + '---');
 }
 
-signup_content_btn.addEventListener("click",registration_content);
-login_content_btn.addEventListener("click",registration_content);
+pages.getAPI = async (api_url) => {
+    try{
+        return await axios(api_url);
+    }catch(error){
+        pages.Console("Error from GET API", error);
+    }
+}
+
+pages.postAPI = async (api_url, api_data, api_token = null) => {
+    try{
+        return await axios.post(
+            api_url,
+            api_data,
+            { headers:{
+                    'Authorization' : "token " + api_token
+                }
+            }
+        );
+    }catch(error){
+        pages.Console("Error from POST API", error);
+    }
+}
+
+pages.loadFor = (page) => {
+    eval("pages.load_" + page + "();");
+}
+
+pages.load_register = async () => {
+    const landing_url = `${pages.baseURL}/register`;
+    const response_landing = await pages.getAPI(landing_url);
+    console.log(response_landing);
+
+    const login_container = document.getElementById("login-content")
+    const signup_container = document.getElementById("signup-content")
+    const login_content_btn = document.getElementById("login-content-button");
+    const signup_content_btn = document.getElementById("signup-content-btn");
+    console.log(login_content_btn);
+
+    const registration_content = () => {
+        login_container.classList.toggle("hide");
+        signup_container.classList.toggle("hide");
+    }
+
+    signup_content_btn.addEventListener("click",registration_content);
+    login_content_btn.addEventListener("click",registration_content);
+}
+
