89ec39933e76fe69c7f87fd8a0f76e468bbaf842
tahataha005 <taha.f.taha0@gmail.com>
2022-10-03 21:46:06 +0300

    Backend: Creating User model

--- dating-server/app/Models/User.php
+++ dating-server/app/Models/User.php
@@ -18,9 +18,16 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
+        'full_name',
+        'username',
         'password',
+        'age',
+        'gender',
+        'interested',
+        'location',
+        'picture',
+        'bio',
+        'incognito'
     ];
 
     /**
