d2ac79077003e8f8d15af61ad9e7a5ccdba54453
tahataha005 <taha.f.taha0@gmail.com>
2022-10-03 16:56:37 +0300

    Backend: Creating favorites table migration

---
+++ dating-server/database/migrations/2022_10_03_061349_create_favorites_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->foreignId("users_id")->nullable()->constrained("id")->on("users");
+            $table->foreignId("favorite_id")->nullable()->constrained("id")->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+};
