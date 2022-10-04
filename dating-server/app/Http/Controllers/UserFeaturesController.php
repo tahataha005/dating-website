393dd97d7abfd1c1ab9fdc81f9ddb70cbab792c0
tahataha005 <taha.f.taha0@gmail.com>
2022-10-04 21:11:00 +0300

    Development: Linking load interested users API and frontend

--- dating-server/app/Http/Controllers/UserFeaturesController.php
+++ dating-server/app/Http/Controllers/UserFeaturesController.php
@@ -14,9 +14,7 @@ class UserFeaturesController extends Controller{
         $interested_gender = User::select("interested")
         ->where("username",$request->username)
         ->get();
-
-        echo $interested_gender[0];
-
+        
         $interested_users = User::select("*")
         ->where("gender",$interested_gender[0]->interested)
         ->get();
