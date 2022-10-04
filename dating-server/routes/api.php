7e7178ff45712ada1e42d5c2dea304c91784cbf3
tahataha005 <taha.f.taha0@gmail.com>
2022-10-04 12:53:05 +0300

    Backend: Creating signup route that calls for signup function in RegistrationController

--- dating-server/routes/api.php
+++ dating-server/routes/api.php
@@ -7,4 +7,5 @@ use App\Http\Controllers\RegistrationController;
 
 // Route::post("/login",[AuthController::class,"login"]);
 
-Route::post("/register",[RegistrationController::class,"login"]);
+Route::post("/login",[RegistrationController::class,"login"]);
+Route::post("/signup",[RegistrationController::class,"signup"]);
