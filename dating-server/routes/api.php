b9269f3875daf48459b41dd704aa9c000d071b1f
tahataha005 <taha.f.taha0@gmail.com>
2022-10-04 18:36:41 +0300

    Backend: Grouping login and signup routes in register route

--- dating-server/routes/api.php
+++ dating-server/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrationController;
 
 
-// Route::post("/login",[AuthController::class,"login"]);
-
-Route::post("/login",[RegistrationController::class,"login"]);
-Route::post("/signup",[RegistrationController::class,"signup"]);
+Route::group(["prefix"=>"register"],function(){
+    Route::post("/login",[RegistrationController::class,"login"]);
+    Route::post("/signup",[RegistrationController::class,"signup"]);
+});
