9596030111e8ca841799e599ca877c651acd8587
tahataha005 <taha.f.taha0@gmail.com>
2022-10-04 18:37:53 +0300

    Backend: Creating UserFeaturesController

---
+++ dating-server/app/Http/Controllers/UserFeaturesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Favorite;
+use App\Models\Message;
+use App\Models\Block;
+
+
+class UserFeaturesController extends Controller{
+    
+}
