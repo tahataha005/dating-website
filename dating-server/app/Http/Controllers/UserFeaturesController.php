5a29c05166d349be7f6fb9fdf092ad7883ee48ee
tahataha005 <taha.f.taha0@gmail.com>
2022-10-04 19:44:46 +0300

    Backend: Editing load_interested_users function to return all interested users in UserFeaturesController

--- dating-server/app/Http/Controllers/UserFeaturesController.php
+++ dating-server/app/Http/Controllers/UserFeaturesController.php
@@ -11,12 +11,14 @@ use App\Models\Block;
 
 class UserFeaturesController extends Controller{
     function load_interested_users(Request $request){
-        $user = User::select("*")
+        $interested_gender = User::select("interested")
         ->where("username",$request->username)
         ->get();
 
+        echo $interested_gender[0];
+
         $interested_users = User::select("*")
-        ->where("gender",$user->interested)
+        ->where("gender",$interested_gender[0]->interested)
         ->get();
 
         return response()->json($interested_users);
