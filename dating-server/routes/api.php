3ca922f5a9d0b98a3134a8cb1fad9fdb377e8b4a
tahataha005 <taha.f.taha0@gmail.com>
2022-10-04 19:46:26 +0300

    Backend: Creating /home route that calls for load_interested_users function from UserFeaturesController

--- dating-server/routes/api.php
+++ dating-server/routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrationController;
-
+use App\Http\Controllers\UserFeaturesController;
 
 Route::group(["prefix"=>"register"],function(){
     Route::post("/login",[RegistrationController::class,"login"]);
     Route::post("/signup",[RegistrationController::class,"signup"]);
 });
+
+Route::post("/home",[UserFeaturesController::class,"load_interested_users"]);
